c9cc13321233d0f3d05b7b53cfe1605f1b3ab2d3
kiseyno92 <kiseyno@Naver.com>
2017-06-05 08:32:21 +0000

    motion.js for second version
    motion.js

--- motion.js
+++ motion.js
@@ -1,29 +1,49 @@
-var Gpio = require('onoff').Gpio;
-var weather = require('./weather.js');
+var Gpio = require('onoff').Gpio,
+    pir = new Gpio(4, 'in', 'both');
+//var weather = require('./weather.js');
 var say = require('say');
+var fs = require('fs');
+var exec_photo = require('child_process').exec;
+var photo_path = '1.jpg';
+var cmd_photo = 'fswebcam -r 1280x720 ' + photo_path;
+var file = fs.readFilesync(photo_path);
+var encodedfile = new Buffer(file).toString('base64');
 
 // exports 안에 코드를 넣어주면 됨
 module.exports = {
   motion: function() {
-    var motion = false, pir = new Gpio(4, 'in', 'both');
+    var motion = false;
 
 	pir.watch(function(err, value) {
-		  if (err) exit();
-			  console.log('Intruder detected');
-			  if(value == 1) {
-          weather.weather('Seoul')
+ 	if (err) exit();
+	say.speak('ready');
+	console.log('Intruder detecting..');
+	if(value == 1) {
+  /*        weather.weather('Seoul')
           .then((weatherSpeak) => {
-            say.speak(weatherSpeak, 'Victoria');
-            isMotion = false;
-            console.log("speak success!!");
-            delay(10000);
-          })
-        }
-	});
+		console.log(weatherSpeak);
+		//voice.korean_say(weatherSpeak);
+		//say.speak(weatherSpeak);		
+  		say.speak(temp);
+		isMotion = false;
+            	console.log("speak success!!");
+            	delay(10000);
+          })*/
 
+	say.speak('capture!!!!');
+	exec_photo(cmd_photo,function(error,stdout,stderr){
+		console.log('Photo Saved');
+});	
+
+	
+	delay(10000);        
+}
+	});
+	say.speak('ready');
 	console.log('Pi Bot deployed successfully!');
 
 	function exit() {
+	console.log('sadfasdfsdf');
 		    pir.unexport();
 			  process.exit();
 	}
